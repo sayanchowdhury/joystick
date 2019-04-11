28d1704003bfb1849e74f194855b15b21d515158
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2019-04-11 16:51:14 +0530

    Add the fedora messaging config file, along with the consumer

---
+++ consumers/fedora_messaging_consumer.py
@@ -0,0 +1,28 @@
+
+
+
+import logging
+
+from fedora_messaging.config import conf
+from fedora_messaging.message import Message
+
+
+_log = logging.getLogger(__name__)
+
+
+class JoyStickController(object):
+    """
+    JoyStick controller for plume.
+    """
+
+    def __init__(self):
+        self.config = conf["consumer_config"]
+
+    def __call__(self, msg):
+        _log.info("Received message from fedora-messaging with topic: %s",
+                 msg.topic)
+        topic = msg.topic
+        body = msg.body
+
+        if topic.endswith("pungi.compose.status.change"):
+            print(message.body)
