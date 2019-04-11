b7bad399fd8dd4f406030d59dc972b9b19334ae5
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2019-04-11 18:09:04 +0530

    Add the logic to pre-release and release the AMIs
    Signed-off-by: Sayan Chowdhury <sayan.chowdhury2012@gmail.com>

--- joystick/consumers/fedora_messaging_consumer.py
+++ joystick/consumers/fedora_messaging_consumer.py
@@ -1,4 +1,5 @@
 import logging
+import subprocess
 
 from fedora_messaging.config import conf
 from fedora_messaging.message import Message
@@ -15,6 +16,25 @@ class JoyStickController(object):
     def __init__(self):
         self.config = conf["consumer_config"]
 
+    def run_command(self, command):
+        _log.info("Starting the command: %r" % command)
+        ret = subprocess.Popen(' '.join(command), stdin=subprocess.PIPE,
+                               shell=True, stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE, close_fds=True)
+        out, err = ret.communicate()
+        retcode = ret.returncode
+        _log.info("Finished executing the command: %r" % command)
+
+        if retcode != 0:
+            _log.error("Command failed during run")
+            _log.error(
+                "(output) %s, (error) %s, (retcode) %s" % (out, err, retcode))
+        else:
+            _log.debug(
+                "(output) %s, (error) %s, (retcode) %s" % (out, err, retcode))
+
+        return out, err, retcode
+
     def __call__(self, msg):
         _log.info("Received message: %s", msg.id)
         topic = msg.topic
@@ -26,10 +46,49 @@ class JoyStickController(object):
                 _log.debug("%s is not a valid status" % msg_info["status"])
                 return
 
-            compose_id = body['compose_id']
-            respin = body['respin']
-            compose_date = body['compose_date']
-            release_version = body['release_version']
+            self.compose_id = body['compose_id']
+            self.respin = body['respin']
+            self.timestamp = body['compose_date']
+            self.release_version = body['release_version']
+
+            success = self._run_pre_release()
+
+            success = self._run_release()
+
+            success = self._publish_messages()
         else:
             _log.debug("Dropping %r" % (topic, msg.id))
             pass
+
+    def _run_pre_release(self):
+        self.run_command([
+            'plume',
+            'pre-release',
+            '--system', 'fedora',
+            '--channel', self.channel,
+            '--version', self.release_version
+            '--timestamp', self.timestamp,
+            '--respin', self.respin,
+            '--board', self.board,
+            "--compose-id", self.compose_id,
+            "--image-type", self.image_type,
+            "--aws-credentials", self.aws_crendentials,
+        ])
+
+    def _run_release(self):
+        self.run_command([
+            'plume',
+            'release',
+            '--system', 'fedora',
+            '--channel', self.channel
+            '--version', self.release_version,
+            '--timestamp', self.timestamp,
+            '--respin', self.respin,
+            '--board',  self.board,
+            '--compose-id', self.compose_id,
+            '--image-type', self.image_type,
+            '--aws-credentials', self.aws_credentials
+        ])
+
+    def _publish_messages(self):
+        raise NotImplementedError
