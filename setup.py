5e4748f03d2b487c0f63f5c54dd04fcc9db5a198
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2019-04-11 17:19:16 +0530

    Rearrange files
    Signed-off-by: Sayan Chowdhury <sayan.chowdhury2012@gmail.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="joystick",
+    version="0.0.1",
+    description="Invoke plume for uploading to cloud",
+    license="LGPLv2+",
+    author="Sayan Chowdhury",
+    author_email="sayan.chowdhury2012@gmail.com",
+    url="https://pagure.io/joystick",
+    packages=find_packages(),
+)
