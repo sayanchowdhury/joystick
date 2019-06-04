eb4e8826262fc7f394963b8e128abdfafdb437ce
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2019-06-05 02:58:58 +0530

    Release 0.1.0
    Signed-off-by: Sayan Chowdhury <sayan.chowdhury2012@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="joystick-py",
-    version="0.1.0",
+    version="0.1.1",
     description="Invoke plume for uploading to cloud",
     license="GPLv3",
     author="Sayan Chowdhury",
